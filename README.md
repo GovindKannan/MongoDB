2239ca01a5ab2b73ca5df1f5f32e24636a3bd090
Govind Kannan <kannangovindgr@gmail.com>
2017-10-29 22:13:25 -0500

    Search and Find test cases added in Mocha

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Title:
 Prerequisites:
   * Set up Mocha
   * Set up Mongo
-  * Create test js files in mocha to connect and create records in mongodb
 
 Overview:
-  In our previous tutorials, we have created a MongoDB (mongotest). In out Mocha test, we have connected to the database and performed a unit test to insert a dummy record into the DB. However each time we run the 'npm run test' command, a new record is inserted into the collection. You may validate this by checking the DB using any tool like Robo 3T.
+  We will be exploring the mongoose methods find(criteria) and findOne(criteria) in this tutorial.
 
-  In this tutorial, we will update the mocha script to add a new hook which will drop the test collection (Drop table) before every test that is executed from mocha.
+  * find()- This method will find all the records that matches the criteria.
+  * findOne() - This will return the first occurance of the record.
 
 Changes made:
   * In connection.js, we will add a new beforeEach hook to drop test collection before each test case is executed in mocha.
--- test/test_CRUD.js
+++ test/test_CRUD.js
@@ -7,20 +7,39 @@ const Model1 = require('../models/model1');
   We first describe the test and add test cases with in our describe block
   */
 describe('CRUD Test to Mongo', function(){
+  var model1JSON;
   //All the tests are defined here
   it('Create Record into Database', function(done){
-    var model1JSON=new Model1({
+    model1JSON=new Model1({
       param1: 'This is a Test record created by Mocha',
       param2: 1
     });
 
     //Save the data to MongoDB using the save method.
     model1JSON.save().then(function(){
-      assert(model1JSON.isNew===false);
-    });//this is an asynchronous process to save the data to MongoDB
+      assert(model1JSON.isNew===false);//returns true only if the record is not existing in DB
 
+    });//this is an asynchronous process to save the data to MongoDB
     done();
   });
 
+  it('Search and Read the record from database', function(done){
+    //Search for records
+    Model1.find().then(function(result){//searches and returns all as no criteria is provided
+      console.log("--------------------------");
+      assert(result.param2===1);
+    });
+    done();
+  });
+
+  it('Search using ID', function(done){
+    //Search for records
+    Model1.findOne({_id:model1JSON._id}).then(function(result){
+      console.log(result.param1);
+      console.log("--------------------------");
+      assert(result._id===model1JSON._id);
+    });
+    done();
+  });
 
 });
